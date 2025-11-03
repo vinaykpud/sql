2ef234c4f87eed283bcb8ad4f608f9c1847cdac9
expani <anijainc@amazon.com>
2025-11-03 07:59:53 -0800

    Making terms agg work for string and date work for substrait schema
    Signed-off-by: expani <anijainc@amazon.com>

--- opensearch/src/main/java/org/opensearch/sql/opensearch/request/OpenSearchQueryRequest.java
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/request/OpenSearchQueryRequest.java
@@ -758,6 +758,7 @@ public class OpenSearchQueryRequest implements OpenSearchRequest {
         });
     }
 
+<<<<<<< HEAD
     private static RelNode convertExtract(RelNode relNode) {
         return relNode.accept(new RelShuttleImpl() {
             @Override
@@ -873,4 +874,6 @@ public class OpenSearchQueryRequest implements OpenSearchRequest {
         });
     }
 
+=======
+>>>>>>> 2dad0763e (Making terms agg work for string and date work for substrait schema)
 }
