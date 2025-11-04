4e329bb54550c65c1a12e50d5c87e58e35474dda
expani <anijainc@amazon.com>
2025-11-04 13:17:25 -0800

    Removed lingering merge conflict
    Signed-off-by: expani <anijainc@amazon.com>

--- opensearch/src/main/java/org/opensearch/sql/opensearch/request/OpenSearchQueryRequest.java
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/request/OpenSearchQueryRequest.java
@@ -756,7 +756,6 @@ public class OpenSearchQueryRequest implements OpenSearchRequest {
         });
     }
 
-<<<<<<< HEAD
     private static RelNode convertExtract(RelNode relNode) {
         return relNode.accept(new RelShuttleImpl() {
             @Override
@@ -872,6 +871,4 @@ public class OpenSearchQueryRequest implements OpenSearchRequest {
         });
     }
 
-=======
->>>>>>> 2dad0763e (Making terms agg work for string and date work for substrait schema)
 }
